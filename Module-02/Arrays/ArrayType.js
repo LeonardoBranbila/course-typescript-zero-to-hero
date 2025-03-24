c623358b74ecc63ec867e48632795e14656f4e1b
LeonardoB <branbila.leo@gmail.com>
2025-03-24 19:32:10 -0300

    To to vídeo 15

---
+++ Module-02/Arrays/ArrayType.js
@@ -0,0 +1,15 @@
+"use strict";
+// let stringArray: string[] = ["zero", "one", "second", "third"];
+// console.log(stringArray);
+// let stringArray2: Array<string> = ["zero", "one", "second", "third"]
+// console.log(stringArray2[2])
+// let stringArray3: string[] = ["Portuguese", "English","Spanish", "Italian"]
+// console.log(stringArray3, "\n")
+// stringArray3.push("Mandarin")
+// console.log(stringArray3)
+let stringArray4 = ["Portuguese", "English", "Spanish", "Italian"];
+console.log('\n' + stringArray4.length);
+// using Spread Operator
+let intArray = [1, 2, 3, 4, 5];
+intArray = [...intArray, 6, 5, 7, 8, 9];
+console.log(intArray);
--- Module-02/Strings/strings.js
+++ Module-02/Strings/strings.js
@@ -1,9 +1,9 @@
 "use strict";
 //Using the back ticks in ts
-let employeeName = 'Leonardo';
+let employeeName = 'Leonardo {}';
 let companyPosition = ".net Developer";
 let companyName = "Company name";
-let employeeData = `Welcome ${employeeName} you will be ${companyPosition} ` + ` in ${companyName}`;
+let employeeData = `Welcome ${employeeName} you will be ${companyPosition} in ${companyName}`;
 console.log('\n' + employeeName);
 console.log(companyPosition);
 console.log(companyName + '\n');
